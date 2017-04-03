5c4a3017448911829aef1b610c8d0c4d22ef6dc3
Tri Nguyen <TriGNguyen@users.noreply.github.com>
2017-04-03 05:21:56 +0000

    completed

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,23 @@
 import pickle
 import tensorflow as tf
+
+# wget https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b432_vgg-100/vgg-100.zip
+# unzip vgg-100.zip
+# wget https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b634_resnet-100/resnet-100.zip
+# unzip resnet-100.zip
+# wget https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b498_inception-100/inception-100.zip
+# unzip inception-100.zip
+# apython3 feature_extraction.py --training_file vgg_cifar10_100_bottleneck_features_train.p --validation_file vgg_cifar10_bottleneck_features_validation.p
+# apython3 feature_extraction.py --training_file resnet_cifar10_100_bottleneck_features_train.p --validation_file resnet_cifar10_bottleneck_features_validation.p
+# apython3 feature_extraction.py --training_file inception_cifar10_100_bottleneck_features_train.p --validation_file inception_cifar10_bottleneck_features_validation.p
+
 # TODO: import Keras layers you need here
+from keras.models import Sequential
+from keras.layers import Input, Dense, Flatten, Activation
+import numpy as np
+
+EPOCHS = 50
+BATCH_SIZE = 256
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -47,8 +64,16 @@ def main(_):
     # 10 for cifar10
     # 43 for traffic
 
-    # TODO: train your model here
+    number_classes = len(np.unique(y_train))
 
+    model = Sequential()
+    model.add(Flatten(input_shape=(X_train.shape[1], X_train.shape[2], X_train.shape[3])))
+    model.add(Dense(number_classes))
+    model.add(Activation('softmax'))
+
+    # TODO: train your model here
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, nb_epoch=EPOCHS, shuffle=True, validation_data=(X_val, y_val))
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
